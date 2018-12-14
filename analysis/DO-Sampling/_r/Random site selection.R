d3a982efac29ff4e15b23fa85bdd61872c05d1a7
Victoria Starnes <vrs93@msstate.edu>
2018-12-14 12:47:40 -0600

    DO sampling points
    A first attempt at sampling site selection for DO.

---
+++ analysis/DO-Sampling/_r/Random site selection.R
@@ -0,0 +1,24 @@
+setwd("C:/Users/Victoria Starnes/Documents/Bluff Lake DO site selection")
+kml.text <- readLines("C:/Users/Victoria Starnes/Documents/Bluff Lake DO site selection/Bluff Lake Draft 1.kml")
+kml.text
+coords <- data.frame(kml.text[51:116])
+coords <- data.frame(do.call('rbind', strsplit(as.character(coords$kml.text.51.116.), ',', fixed = TRUE)))
+str(coords)
+coords$X1 <- as.numeric(as.character(coords$X1))
+coords$X2 <- as.numeric(as.character(coords$X2))
+coords$X3 <- as.numeric(as.character(coords$X3))
+colnames(coords) <- c('x','y','z')
+str(coords)
+      
+write.table(coords, "poly_coordinates.csv", sep = ",", row.names = F)
+    
+library(sp)
+xy <- cbind(coords$x, coords$y)
+plot(xy, type = 'l')
+xy.poly <- Polygon(xy)
+xy.points.reg <- spsample(xy.poly, n = 23, type = "regular") # n is sample size
+set.seed(123)
+xy.points.nonal.20 <- spsample(xy.poly, n = 23, type = "nonaligned") # n is sample size
+plot(xy.points.nonal.20, add = TRUE, pch = 3)
+
+write.table(xy.points, "point_coordinates.csv", sep = ",", row.names = F) 
