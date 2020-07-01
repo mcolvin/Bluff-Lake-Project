ed9e74bd55b1f72cae84f9a5e162def1fd212cba
mcolvin <colvin.mike@gmail.com>
2020-07-01 17:00:01 -0500

    data wrangling for lake model calibration

---
+++ _analysis/bluff-lake-model/_r/3-load-and-clean.R
@@ -0,0 +1,75 @@
+
+#----------------------------------------------------------------------
+# 
+#  NOXUBEE RIVER DAILY DISCHARGE DATA
+#
+#----------------------------------------------------------------------
+
+# PULL DISCHARGE AND GAUGE DATA FROM USGS PORTAL
+## PARAMETER CODE 00065 Gage height, feet 00060; Discharge, cubic feet per second
+discharge_daily<-fread("_dat/discharge_daily.csv")
+discharge_daily[,date:=as.Date(date)]
+tmp<-as.numeric(Sys.Date()-as.Date(max(discharge_daily$date)))
+if(tmp>15)# pull data again if more than 15 days have passed since last pull
+    {
+    current_date<- Sys.Date()
+    path<-paste0("https://waterdata.usgs.gov/nwis/dv?cb_00060=on&cb_00065=on&format=rdb&site_no=02448000&referred_module=sw&period=&begin_date=1938-09-21&end_date=",current_date)
+    discharge_daily <- read.table(url(path),sep="\t",skip = 32,header=FALSE)
+    names(discharge_daily)<- c("agency_cd","site_no","datetime","gage","78518_00065_00003_cd",
+        "discharge","78519_00060_00003_cd")
+    discharge_daily<-as.data.table(discharge_daily)
+    discharge_daily[,date:=as.Date(datetime)]
+    discharge_daily[,year:=as.numeric(format(date,"%Y"))]
+    discharge_daily[,doy:=as.numeric(format(date,"%j"))]
+    write.csv(discharge_daily,"_dat/discharge_daily.csv")
+    }
+
+#----------------------------------------------------------------------
+# 
+#  NOXUBEE RIVER DISCHARGE DATA: HOURLY
+#
+#----------------------------------------------------------------------
+
+# PULL DISCHARGE AND GAUGE DATA FROM USGS PORTAL
+## PARAMETER CODE 00065 Gage height, feet 00060; Discharge, cubic feet per second
+discharge_hourly<-fread("_dat/discharge_hourly.csv")
+discharge_hourly[,date:=as.Date(date)]
+tmp<-as.numeric(Sys.Date()-as.Date(max(discharge_hourly$date)))
+if(tmp>15)# pull data again if more than 15 days have passed since last pull
+    {
+    
+    discharge_hourly <- dataRetrieval::readNWISuv(siteNumbers = "02448000",
+        parameterCd = c("00060","00065"),
+        startDate = as.Date("1986-10-10"),
+        endDate = Sys.Date(),
+        tz="America/Chicago")
+    names(discharge_hourly)[4]<-"gage"
+    names(discharge_hourly)[6]<-"discharge"
+    discharge_hourly<-as.data.table(discharge_hourly)
+    discharge_hourly[,date:=as.Date(datetime)]
+    discharge_hourly[,year:=as.numeric(format(date,"%Y"))]
+    discharge_hourly[,doy:=as.numeric(format(date,"%j"))]
+    write.csv(discharge_hourly,"_dat/discharge_hourly.csv")
+    }
+
+#----------------------------------------------------------------------
+# 
+#  WATER LEVEL LOGER DATA
+#
+#----------------------------------------------------------------------
+
+
+
+#----------------------------------------------------------------------
+# 
+#  BATHYMETRIC DATA
+#
+#----------------------------------------------------------------------
+
+bath<- fread("_dat/Bathymetry/CompleteMap.csv")
+names(bath)<-c("X","Y","elevation")
+
+
+
+
+
---
+++ _analysis/bluff-lake-model/readme.md
@@ -0,0 +1,10 @@
+# Bluff Lake Water Level calibration
+
+This directory uses water level data and discharge
+data to calibrate the lake water levels to 
+inflows and outflows. 
+
+
+
+
+
