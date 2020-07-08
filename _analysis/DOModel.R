c27e075ba854f4d43e64ccbecb1c113a1f1add96
Victoria Starnes <vrs93@msstate.edu>
2020-07-08 14:00:44 -0500

    DO Model

--- _analysis/DOModel.R
+++ _analysis/DOModel.R
@@ -4,44 +4,37 @@ library(raster)
 library(rgdal)
 library(rgeos)
 library(ggplot2)
-
-#Water Respiration----
-dat<-read.csv("DO-Sampling/_dat/Data/WC_Respiration_June15_16_2020.csv")
-Dark<-subset(dat, ï..SampleType=="Dark")
-Dark$StartTime<-as.POSIXct(Dark$StartTime, format="%m/%d/%Y %H:%M")
-Dark$EndTime<-as.POSIXct(Dark$EndTime, format="%m/%d/%Y %H:%M")
-Dark$DiffTime<-Dark$EndTime-Dark$StartTime
-Dark$DiffTime<-as.numeric(Dark$DiffTime)
-Dark$WCresp<-(Dark$InitialDO-Dark$EndDO)/Dark$DiffTime
-EndDO<-Dark%>%dplyr::group_by(as.factor(Samplelocation), DepthofCollection)%>%dplyr::summarize(EndDO=mean(EndDO), StartDO=mean(InitialDO),DiffTime=mean(DiffTime))
-EndDO$DiffTime<-as.numeric(EndDO$DiffTime)
-EndDO$WCresp<-(EndDO$StartDO-EndDO$EndDO)/EndDO$DiffTime
-
-
-WCresp<-0.07203515 #lake average
+#DO Model 1----
+WCresp<-0.07203515 #lake average mg/L/hr
 tempC<-25
-#Sediment Respiration----
-SOU<-(((0.287*tempC-2.5)*44.661)*.7)*0.001
+SOU<-(((0.287*tempC-2.5)*44.661)*.7)*0.001 #Sediment Respiration SOU/hr
 dat2 <- read.csv("Depth-Mapping/_dat/Bathymetry/WCS_BTTMUP_2_2.csv")
-dat2$depth<- 66.52752-dat2$POINT_Z
-dat2$depth<-ifelse(dat2$depth<=0, 0, dat2$depth)
-DOdusk<-10
-Lengthnight<-10
-#DO Model----
+dat2$depth<- 66.52752-dat2$POINT_Z #9 boards in the WCS
+dat2$depth<-ifelse(dat2$depth<=0, 0, dat2$depth) #remove dry areas
+DOdusk<-10 #mg/L 
+Lengthnight<-10 #hrs
 dat2$DOdawn<-DOdusk-(Lengthnight*((SOU/dat2$depth)+WCresp))
 dat2$DOdawn<-ifelse(dat2$DOdawn<=0, 0, dat2$DOdawn)
 
 #DO Model 2----
-dat2 <- read.csv("Depth-Mapping/_dat/Bathymetry/WCS_BTTMUP_2_2.csv")
-WCresp<-0.07203515/60 #lake average
+dat2 <- read.csv("Depth-Mapping/_dat/Bathymetry/WCS_BTTMUP_2_2.csv")\
+dat2$depth<- 68.39712-dat2$POINT_Z
+WR<-0.07203515/60 #lake average water respiration in mg/L/min
 tempC<-25
-SOU<-((((0.287*tempC-2.5)*44.661)*.7)*0.001)/60
-DOdusk<-10
-Lengthnight<-10*60
-DOsat<-4.09+(10.5*exp(-0.0371*25))
-DE<-(DOdusk*DOsat)*(2.2*10^-5)*(0.03^-1)*60*10^-2
-dat2$Dawn<-DOdusk-(((SOU*dat2$depth^-1)+WCresp+(DE*dat2$depth^-1))*Lengthnight)
-dat2$DOdawn<-ifelse(dat2$DOdawn<=0, 0, dat2$DOdawn)
+SR<-((((0.287*tempC-2.5)*44.661)*.7)*0.001)/60 #Same as SOU above just SOU/min
+DOdusk<-10 #mg/L
+DOsat<-4.09+(10.5*exp(-0.0371*tempC)) #DO Saturation at a given temperature
+DE<-(DOdusk*DOsat)*(2.2*10^-5)*(0.03^-1)*60*10^-2 #diffusive exchange
+Z<-dat2$depth #depth
+#Integration
+eq_2<-function(DO_t){DO_t-SR*Z^-1 + WR +DE*Z^-1
+#Or it might look like this depending on how integrate 
+  #takes the state variable DO
+eq_2_2<-function(DO_t){-SR*Z^-1 + WR +DE*Z^-1}
+#integrate function
+do_dawn=DOdusk-integrate()
+
+
 
 
 library(gstat)
