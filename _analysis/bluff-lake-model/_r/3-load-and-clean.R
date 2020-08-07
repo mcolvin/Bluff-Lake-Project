e3b22c4e7039779633cf6e624d96477e82df9c24
Victoria Starnes <vrs93@msstate.edu>
2020-08-07 15:40:00 -0500

    bridge/notch function added
    still need to add a few more elevations

--- _analysis/bluff-lake-model/_r/3-load-and-clean.R
+++ _analysis/bluff-lake-model/_r/3-load-and-clean.R
@@ -86,7 +86,31 @@ macon<- macon*2.58999 # mi^2
 bath<- fread("_dat/Bathymetry/CompleteMap.csv")
 names(bath)<-c("X","Y","elevation")
 
+#----------------------------------------------------------------------
+# 
+#  Bridges and Notches
+#
+#----------------------------------------------------------------------
 
+In_out<-function(location,wse)
+{
+  if(location==1 & WSE<67.39) {x<-0)# no water
+if(location==1 & WSE>67.39) {x<-1)# water moving in
 
+if(location==2 & WSE>68.11) {x<-1}# water moving out
+if(location==2 & WSE<68.11) {x<-0)# no water
+if(location==2 & WSE>???) {x<-1)# water moving in
 
+if(location==3 & WSE<68.04) {x<-0)# no water
+if(location==3 & WSE>68.04) {x<-1)# water moving in
+
+if(location==4 & WSE<68.23) {x<-0)# no water
+if(location==4 & WSE>68.23) {x<-1)# water moving in
+
+if(location==5 & WSE>???) {x<-1}# water moving out
+if(location==5 & WSE<68.82) {x<-0)# no water
+if(location==5 & WSE>68.82) {x<-1)# water moving in
+
+return(x)
+}
 
