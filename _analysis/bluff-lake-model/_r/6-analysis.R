5adfcc413c0287c10ec84c7f64ee302f7fcf9869
mcolvin <colvin.mike@gmail.com>
2020-09-01 21:29:23 -0500

    Update 6-analysis.R

--- _analysis/bluff-lake-model/_r/6-analysis.R
+++ _analysis/bluff-lake-model/_r/6-analysis.R
@@ -9,38 +9,33 @@ wse_dyn<-function(t,x,parms)
     V<-x[1]
     # convert lake volume to water surfac elevation
     wse<- Vol_2_EL(V)
-    # surface area
+    # lake surface area
     sa<- Vol_2_SA(V)
-    
-    
-    
-    
-    lake_area<- 0# f(V)
+    # acceleration due to gravity
     acc_due_to_gravivity<- (2*32)^0.5
     # water coming into the lake from the Noxubee
     V_inn_1<- 1    
     # water releasing over the WCS
-    board<-0
-    wcs_head<-0 
-    V_inn_2<-1
-    V_inn_3<-1
-    em_spill<-1
-    wcs<-1
-    
-    dV<- 1#inn_1 + inn_2 + inn_3 - em_spill - wcs
-    return(list(V=dV,wse=wse))
+    board<-0# Board_Time(ttt) need to link hour to doy
+
+    # this adds 1 m^3/hr 
+    dV<- 1
+    return(list(V=dV,wse=wse, sa=sa))
     }
+# initialize lake volume in m^3
 ini_values<-c(258323640)
-parameters<-NULL
+parameters<-NULL # no parameters yet
 solution<- deSolve::ode(
     y=ini_values, 
     times=c(0:(10*60)), 
     func=wse_dyn, 
     parms=parameters, 
     method="rk4")
-colnames(solution) <- c("time", "V", "wse")
+colnames(solution) <- c("time", "V", "wse","sa")
 solution<-as.data.table(solution)
-plot(solution,ylab="DO mg/L",las=1,main="")
+plot(V~t,solution,ylab="Lake volume",las=1,main="")
+plot(sa~t,solution,ylab="Lake surface area",las=1,main="")
+plot(wse~t,solution,ylab="Lake water surface elevation",las=1,main="")
 
 
 
