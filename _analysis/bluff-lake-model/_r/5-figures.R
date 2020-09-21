6aa5e603f700cc9c5a213230b13985679974bbdb
mcolvin <colvin.mike@gmail.com>
2020-09-21 15:00:21 -0500

    Update 5-figures.R

--- _analysis/bluff-lake-model/_r/5-figures.R
+++ _analysis/bluff-lake-model/_r/5-figures.R
@@ -19,21 +19,27 @@ figures<- function(n)
 
     if(n==2)
         {
-        plot(Intake~discharge_cms,lake_info,type='p',ylab="Water surface elevation")
-        plot(Cypress~cont_time,lake_info,type='l',ylab="Water surface elevation",
-            ylim=c(67.5,70.5))
-        points(Gauge~cont_time,lake_info,type='l',col="red")
-        points(Intake~cont_time,lake_info,type='l',col="green")
+        plot(Intake~cont_time,lake_info,type='l',ylab="Water surface elevation")
         par(new=TRUE)
         plot(discharge_cms~cont_time,lake_info,type='l',yaxt="n",ylab='',
-            col="lightgrey")
+            col="lightgrey",lwd=3)
         axis(side=4, at=axTicks(2),labels=TRUE)
         mtext(side=4,"Discharge")
-             abline(v=125000)       
-        legend("topleft",legend=c("Cypress","Gauge","Intake","Macon gauge"),
-            col=c("black","red","green","lightgrey"),lwd=3,bg="white")
+        legend("topleft",legend=c("Intake","Macon gauge"),
+            col=c("black","lightgrey"),lwd=3,bg="white")
+
         } 
-        
+    if(n=="macon-intake")
+        {
+        fit<- lm(Intake~log(discharge_cms),lake_info)  
+        lake_info$pred<-predict(fit,lake_info)
+        par(mfrow=c(2,1))
+        #plot(Cypress~discharge_cms,lake_info)  
+        #plot(Cypress~log(discharge_cms),lake_info)  
+        plot(Intake~log(discharge_cms),lake_info)  
+        plot(pred~Intake,lake_info)
+        abline(0,1)
+        }
     if(n=="bluff-lake-daily-q-input")
         {
         # plot of discharge scaled to watershed size
