3152958f99c65d89d3655784ac37560f73cbb1a5
mcolvin <colvin.mike@gmail.com>
2020-10-01 16:32:34 -0500

    zoo na.aprox issue

--- _analysis/bluff-lake-model/_r/3-load-and-clean.R
+++ _analysis/bluff-lake-model/_r/3-load-and-clean.R
@@ -187,7 +187,7 @@ lake_info[,Intake:=ifelse(is.nan(Intake),NA,Intake)]
 #----------------------------------------------------------------------
 ## data from intake is the limiting value
 model_data<-as.data.table(subset(lake_info, dt> as.POSIXct("2019-11-12 11:30:00")))
-model_data$Intake<-dplyr::na.approx(model_data$Intake)
+model_data$Intake<-zoo::na.approx(model_data$Intake)
 model_data[,cont_time:=cont_time-min(cont_time)]
 # wse_intake
 wse_intake<-approxfun(model_data$cont_time,
