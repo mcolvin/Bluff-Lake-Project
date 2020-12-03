e68ec22a07da75baa6f3329b98ad6f126d4c24bd
Caleb Aldridge <caa134@msstate.edu>
2020-12-02 20:34:38 -0600

    Added scaffolding
    Added scaffolding for tabs and information.

--- app/bluff-lake-fisheries-shinydashboard.R
+++ app/bluff-lake-fisheries-shinydashboard.R
@@ -18,65 +18,94 @@ lapply(packs, library, character.only = TRUE)
 
 # ui.R --------------------------------------------------------------------
 
-dashboardPage(dashboardHeader(),
-              dashboardSidebar(),
-              dashboardBody())
+# Header content
+header <- dashboardHeader(title = "Bluff Lake Fisheries Dashboard")
+
+# Sidebar content
+sidebar <- dashboardSidebar(sidebarMenu(
+    menuItem(
+        "Overview and description",
+        tabName = "overview",
+        icon = icon("fish")
+    ),
+    menuItem(
+        "Bathymetry",
+        tabName = "bathymetry",
+        icon = icon("atlas")
+    ),
+    menuItem(
+        "Objectives",
+        tabName = "objectives",
+        icon = icon("bullseye")
+    ),
+    menuItem(
+        "Water releases",
+        tabName = "water_releases",
+        icon = icon("water")
+    )
+))
+
+# Body content
+body <- dashboardBody(tabItems(
+    # Overview tab
+    tabItem(tabName = "overview",
+            h2("Overview and description")),
+    
+    # Dashboard tab
+    tabItem(tabName = "bathymetry",
+            h2("Bathymetry")),
+    
+    # Objectives tab
+    tabItem(tabName = "objectives",
+            h2("Objectives")),
+    
+    # Bath tab
+    tabItem(tabName = "water_releases",
+            h2("Water releases"),
+            fluidRow(
+                
+                # Plot box
+                box(plotOutput("histPlot", height = 250)),
+                
+                # Inputs box
+                box(
+                    title = "Inputs",
+                    sliderInput(
+                        inputId = "nSlider",
+                        label = "N:",
+                        min = 10,
+                        max = 1000,
+                        value = 100,
+                        step = 10
+                    ),
+                    sliderInput(
+                        inputId = "meanSlider",
+                        label = "Mean:",
+                        min = 25,
+                        max = 75,
+                        value = 50
+                    ),
+                    sliderInput(
+                        inputId = "stdDevSlider",
+                        label = "SD:",
+                        min = 1,
+                        max = 15,
+                        value = 5
+                    ),
+                    sliderInput(
+                        inputId = "binSlider",
+                        label = "Bins:",
+                        min = 2,
+                        max = 20,
+                        value = 10
+                    )
+                )
+            ))    
+))
+
+ui <- dashboardPage(header, sidebar, body)
 
 # app.R -------------------------------------------------------------------
-
-ui <- dashboardPage(
-    # Header content
-    dashboardHeader(title = "Bluff Lake Fisheries Dashboard"),
-    
-    # Sidebar content
-    dashboardSidebar(sidebarMenu(
-        menuItem(
-            "Dashboard",
-            tabName = "dashboard",
-            icon = icon("dashboard")
-        )
-    )),
-    
-    # Body content
-    dashboardBody(fluidRow(
-        # Plot box
-        box(plotOutput("histPlot", height = 250)),
-        
-        # Inputs box
-        box(
-            title = "Inputs",
-            sliderInput(
-                inputId = "nSlider",
-                label = "N:",
-                min = 10,
-                max = 1000,
-                value = 100,
-                step = 10
-            ),
-            sliderInput(
-                inputId = "meanSlider",
-                label = "Mean:",
-                min = 25,
-                max = 75,
-                value = 50
-            ),
-            sliderInput(
-                inputId = "stdDevSlider",
-                label = "SD:",
-                min = 1,
-                max = 15,
-                value = 5
-            ),
-            sliderInput(
-                inputId = "binSlider",
-                label = "Bins:",
-                min = 2,
-                max = 20,
-                value = 10
-            )
-        )
-    ))
-)
 
 server <- function(input, output) {
     set.seed(20201201)
@@ -91,7 +120,7 @@ server <- function(input, output) {
         
         # Build plot
         # histData <- normData[seq_len(input$binSlider)]
-        hist(normData, 
+        hist(normData,
              breaks = input$binSlider,
              xlim = c(0, 100))
     })
