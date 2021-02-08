9bf97294a062ab79619e1e61516421359cf900b1
Michael E. Colvin <mec685@msstate.edu>
2021-02-08 07:12:51 -0600

    big sims
    results of running the big simulation

--- _analysis/bluff-lake-model/index.r
+++ _analysis/bluff-lake-model/index.r
@@ -4,14 +4,14 @@ this_dir <- function(directory)
 setwd(file.path(getwd(), directory) )
 # end
 
-source("_r/1-global.R")
+source("_r/1-global.R");library(scales);library(tidyverse)
 source("_r/2-functions.R")
 source("_r/3-load-and-clean.R")
 source("_r/4-tables.R")
 source("_r/5-figures.R")
-source("_r/6-analysis.R")
+source("_r/6.5-analysis-hour-edt-all-elevations.R")
 
-
+install.packages("tidyverse")
 # figures
 
 # tables
